c10b8acd4e58ecd33b6e2145be327e4d58ebb893
sashka <44439409+litt1e@users.noreply.github.com>
2019-06-30 15:44:23 +0300

    Update bot_action.py
    new function

--- comm_bot/bot/bot_action.py
+++ comm_bot/bot/bot_action.py
@@ -1,17 +1,27 @@
+#*- coding: utf-8 -*-
 from comm_bot import models
 
 
 def create_user(name, login, user_id, description='блохир'):
-    models.User.objects.create(name=name, login=login, id=user_id, description=description)# TODO: мб сюда придётца пкакую-нибдуь проверочку пришпендюрить
+    try:
+        user = models.User.objects.get(id=user_id)
+    except models.User.DoesNotExist:
+        user = models.User.objects.create(name=name,
+                                          login=login,
+                                          id=user_id,
+                                          description=description)
+    return user, 'created'
 
 
 def create_post(user_id, title, text):
     user = models.User.objects.get(id=user_id)
-    post = models.Thread.objects.create(user=user, title=title, text=text)
-    return post.id
+    post = models.Thread.objects.create(user=user,
+                                        title=title,
+                                        text=text)
+    return post
 
 
-def delete_post(user_id, thread_id):
+def delete_post(thread_id):
     thread = models.Thread.objects.get(id=thread_id)
     thread.delete()
 
@@ -21,14 +31,33 @@ def self_destruction(user_id):
     login = user.login
     name = user.name
     user.delete()
-    models.User.objects.create(id=user_id, login=login, name=name, description='все посты были самоуничтожены!1!1!')
+    models.User.objects.create(id=user_id,
+                               login=login,
+                               name=name,
+                               description='все посты были самоуничтожены!1!1!')
 
 
 def get_position(user_id):
-    return models.User.objects.get(id=user_id).position
+    try:
+        pos = models.User.objects.get(id=user_id).position
+    except models.User.DoesNotExist:
+        pos = 'nothing'
+        return pos
+    else:
+        return pos
+
 
 
 def set_position(user_id, position):
     user = models.User.objects.get(id=user_id)
     user.position = position
     user.save()
+
+
+def is_there_login(login):
+    try:
+        models.User.objects.get(login=login)
+    except models.User.DoesNotExist:
+        return False
+    else:
+        return True
