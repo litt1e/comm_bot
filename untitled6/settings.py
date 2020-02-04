b6444a82501772fd743c5bf3a1c17c2208bbf375
sashka <44439409+litt1e@users.noreply.github.com>
2020-02-04 20:54:21 +0300

    Update settings.py

--- untitled6/settings.py
+++ untitled6/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comm_bot.apps.CommBotConfig'
+    'comm_bot.apps.CommBotConfig',
+    'herokuapp'
 ]
 
 MIDDLEWARE = [
